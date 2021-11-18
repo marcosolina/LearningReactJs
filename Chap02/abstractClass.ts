5058c5c138b2961d38d1e2726cc9bd8ef7f9429f
Marco Solina <marco.solina@ixico.com>
2021-11-18 09:55:57 +0100

    progress

---
+++ Chap02/abstractClass.ts
@@ -0,0 +1,43 @@
+namespace AbstractNamespace {
+
+    abstract class Vehicle {
+        constructor(protected wheelCount: number){}
+
+        abstract updateWheelCount(newWheelCount: number) : void;
+
+        showNumberOfWheels(){
+            console.log(`moved ${this.wheelCount} miles`);
+        }
+    }
+
+    class Motorcycle extends Vehicle{
+        constructor(){
+            super(2);
+        }
+
+        updateWheelCount(newWheelCount: number){
+            this.wheelCount = newWheelCount;
+            console.log(`Motorcycle has ${this.wheelCount}`);
+        }
+    }
+
+    class Automobile extends Vehicle{
+        constructor(){
+            super(4);
+        }
+
+        updateWheelCount(newWheelCount: number){
+            this.wheelCount = newWheelCount;
+            console.log(`Automibile has ${this.wheelCount}`);
+        }
+    }
+
+    
+    const motorCycle = new Motorcycle();
+    motorCycle.showNumberOfWheels();
+    motorCycle.updateWheelCount(1);
+    motorCycle.showNumberOfWheels();
+    
+    const autoMobile = new Automobile();
+    autoMobile.showNumberOfWheels();
+}
---
+++ Chap02/namespaces.ts
@@ -0,0 +1,8 @@
+namespace A {
+    class FirstClass {}
+}
+
+namespace B {
+    class SecondClass {}
+    //const test = new FirstClass(); // non compila perchè è in un namespace diverso
+}
